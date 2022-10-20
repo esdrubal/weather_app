9a74864b1b208c7041fd09574add3378a7ec3635
marcos henrich <marcoshenrich@gmail.com>
2022-10-20 14:52:21 +0100

    Fixes babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,10 @@
 module.exports = {
   presets: [
     '@babel/preset-typescript',
-    '@babel/preset-react',
+    [
+      '@babel/preset-react',
+      {runtime: 'automatic'}
+    ],
     [ 
       '@babel/preset-env',
       {targets: {node: 'current'}}
